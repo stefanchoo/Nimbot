b4ac4456b64f039eff5046dbf97d8a907b5d15cf
朱沪生 <hszhu@zdmedtech.com>
2017-02-27 13:57:53 +0800

    create a project

---
+++ src/main/java/com/zdmedtech/nimbot/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.zdmedtech.nimbot.config.audit;
---
+++ src/main/java/com/zdmedtech/nimbot/web/rest/util/HeaderUtil.java
@@ -0,0 +1,45 @@
+package com.zdmedtech.nimbot.web.rest.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+
+/**
+ * Utility class for HTTP headers creation.
+ */
+public final class HeaderUtil {
+
+    private static final Logger log = LoggerFactory.getLogger(HeaderUtil.class);
+
+    private static final String APPLICATION_NAME = "nimbotApp";
+
+    private HeaderUtil() {
+    }
+
+    public static HttpHeaders createAlert(String message, String param) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-nimbotApp-alert", message);
+        headers.add("X-nimbotApp-params", param);
+        return headers;
+    }
+
+    public static HttpHeaders createEntityCreationAlert(String entityName, String param) {
+        return createAlert(APPLICATION_NAME + "." + entityName + ".created", param);
+    }
+
+    public static HttpHeaders createEntityUpdateAlert(String entityName, String param) {
+        return createAlert(APPLICATION_NAME + "." + entityName + ".updated", param);
+    }
+
+    public static HttpHeaders createEntityDeletionAlert(String entityName, String param) {
+        return createAlert(APPLICATION_NAME + "." + entityName + ".deleted", param);
+    }
+
+    public static HttpHeaders createFailureAlert(String entityName, String errorKey, String defaultMessage) {
+        log.error("Entity creation failed, {}", defaultMessage);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-nimbotApp-error", "error." + errorKey);
+        headers.add("X-nimbotApp-params", entityName);
+        return headers;
+    }
+}
